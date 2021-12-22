cc8e2f6a1215441578a7454f83860206e14b9bc7
Philipe Santos <philipe_02@hotmail.com>
2021-12-22 16:04:19 -0300

    adjustments to deploy

--- product/index.php
+++ product/index.php
@@ -13,8 +13,6 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-error_log('endpoint ' . $uri[1]);
-
 // all of our endpoints start with /product
 // everything else results in a 404 Not Found
 if ($uri[1] !== 'product') {
@@ -27,12 +25,8 @@ if (isset($uri[2])) {
     $productSku = $uri[2];
 }
 
-
-
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $body = file_get_contents('php://input');
-
-error_log(LOG_DEBUG, $_SERVER['REQUEST_URI'] . ' ' . $requestMethod);
 
 $controller = new ProductController($requestMethod, $productSku, $body);
 $controller->processRequest();
